0cf10713415fb12c4d03f527650568eb507d42aa
blakehulett7 <blake.hulett7@gmail.com>
2025-01-19 17:20:30 -0600

    Gotta remember this car fleet

--- main.go
+++ main.go
@@ -11,46 +11,22 @@ func fmtin() {
 	fmt.Println()
 }
 
-type Stack [][2]int
+type Stack []int
 
-func (s *Stack) push(num_idx [2]int) {
-	*s = append(*s, num_idx)
+func (s *Stack) push(num int) {
+	*s = append(*s, num)
 }
 
-func (s *Stack) pop() [2]int {
+func (s *Stack) pop() int {
 	top := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
 	return top
 }
 
-func (s Stack) peek() [2]int {
+func (s Stack) peek() int {
 	return s[len(s)-1]
 }
 
-func dailyTemperatures(temperatures []int) []int {
-	stack := Stack{}
-	result := make([]int, len(temperatures))
+func carFleet(target int, position, speed []int) int {
 
-	idx := 0
-	for idx < len(temperatures) {
-		temp := temperatures[idx]
-
-		if len(stack) == 0 {
-			stack.push([2]int{temp, idx})
-			idx++
-			continue
-		}
-
-		prev_temp := stack.peek()[0]
-		if temp <= prev_temp {
-			stack.push([2]int{temp, idx})
-			idx++
-			continue
-		}
-
-		prev_idx := stack.pop()[1]
-		result[prev_idx] = idx - prev_idx
-	}
-
-	return result
 }
