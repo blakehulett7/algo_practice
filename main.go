c9d17a00c150c5c28d0ab2e8d3698dd661cb94a7
blakehulett7 <blake.hulett7@gmail.com>
2025-01-19 16:36:37 -0600

    Got it! Still gonna mess with generics in the future though

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"strings"
 )
 
 func main() {
@@ -13,19 +12,19 @@ func fmtin() {
 	fmt.Println()
 }
 
-type Stack []any
+type Stack []int
 
-func (s *Stack) push(element any) {
+func (s *Stack) push(element int) {
 	*s = append(*s, element)
 }
 
-func (s *Stack) pop() any {
+func (s *Stack) pop() int {
 	top := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
 	return top
 }
 
-func (s Stack) peek() any {
+func (s Stack) peek() int {
 	return s[len(s)-1]
 }
 
@@ -33,27 +32,28 @@ func scratch(tokens []string) int {
 	stack := Stack{}
 
 	for _, token := range tokens {
+		fmt.Printf("stack: %s, token: %s", stack, token)
 		switch token {
 		case "+":
-			num_1 := stack.pop().(int)
-			num_2 := stack.pop().(int)
+			num_1 := stack.pop()
+			num_2 := stack.pop()
 			stack.push(num_1 + num_2)
 		case "-":
-			num_1 := stack.pop().(int)
-			num_2 := stack.pop().(int)
+			num_1 := stack.pop()
+			num_2 := stack.pop()
 			stack.push(num_1 - num_2)
 		case "*":
-			num_1 := stack.pop().(int)
-			num_2 := stack.pop().(int)
+			num_1 := stack.pop()
+			num_2 := stack.pop()
 			stack.push(num_1 * num_2)
 		case "/":
-			num_1 := stack.pop().(int)
-			num_2 := stack.pop().(int)
-			stack.push(num_1 / num_2)
+			num_1 := stack.pop()
+			num_2 := stack.pop()
+			stack.push(num_2 / num_1)
 		default:
 			num, _ := strconv.Atoi(token)
 			stack.push(num)
 		}
 	}
-	return stack.pop().(int)
+	return stack.pop()
 }
