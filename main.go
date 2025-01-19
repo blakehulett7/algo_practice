f5ecd9dfd73b4dde1d4e070bb44670db18dc4d67
blakehulett7 <blake.hulett7@gmail.com>
2025-01-19 17:33:01 -0600

    Gotta sort by position

--- main.go
+++ main.go
@@ -11,22 +11,44 @@ func fmtin() {
 	fmt.Println()
 }
 
-type Stack []int
+type Stack []float64
 
-func (s *Stack) push(num int) {
+func (s *Stack) push(num float64) {
 	*s = append(*s, num)
 }
 
-func (s *Stack) pop() int {
+func (s *Stack) pop() float64 {
 	top := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
 	return top
 }
 
-func (s Stack) peek() int {
+func (s Stack) peek() float64 {
 	return s[len(s)-1]
 }
 
+// Need to sort this by position first
 func carFleet(target int, position, speed []int) int {
+	stack := Stack{}
 
+	cars := [][2]int{}
+	for i := 0; i < len(position); i++ {
+		cars = append(cars, [2]int{position[i], speed[i]})
+	}
+
+	for _, car := range cars {
+		time := float64(target-car[0]) / float64(car[1])
+
+		if len(stack) == 0 {
+			stack.push(time)
+			continue
+		}
+
+		prev_time := stack.peek()
+		if time < prev_time {
+			stack.push(time)
+		}
+	}
+
+	return len(stack)
 }
