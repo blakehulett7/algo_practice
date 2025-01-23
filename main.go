32780eda8becfa900d137fb15759288ae9f66df5
blakehulett7 <blakehulett@Blake-Huletts-MacBook-Pro.local>
2025-01-23 12:04:00 -0600

    keymaps:

--- main.go
+++ main.go
@@ -12,26 +12,32 @@ func fmtin() {
 	fmt.Println()
 }
 
-type Stack []int
+type Stack [][]int
 
-func (s *Stack) push(num int) {
+func (s *Stack) push(num []int) {
 	*s = append(*s, num)
 }
 
-func (s *Stack) pop() int {
+func (s *Stack) pop() []int {
 	top := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
 	return top
 }
 
-func (s Stack) peek() int {
+func (s Stack) peek() []int {
 	return s[len(s)-1]
 }
 
 func largestRectangleArea(heights []int) int {
 	stack := Stack{}
 
-	for idx, height := range heights {
+	idx := 0
+	for idx < len(heights) {
+		height := heights[idx]
+		if len(stack) == 0 {
+			stack.push([]int{idx, height})
+			continue
+		}
 
 	}
 }
