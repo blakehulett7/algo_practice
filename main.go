00e675d1f1c82cf5ab11737df0a78e9945643657
blakehulett7 <blakehulett@Blake-Huletts-MacBook-Pro.local>
2025-01-24 12:37:06 -0600

    close

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func (s Stack) peek() []int {
 }
 
 func largestRectangleArea(heights []int) int {
+	largest := 0
 	stack := Stack{}
 
 	idx := 0
@@ -36,8 +37,34 @@ func largestRectangleArea(heights []int) int {
 		height := heights[idx]
 		if len(stack) == 0 {
 			stack.push([]int{idx, height})
+			idx++
+			continue
+		}
+		prev_height := stack.peek()[1]
+		if prev_height <= height {
+			stack.push([]int{idx, height})
+			idx++
 			continue
 		}
 
+		prev_idx := stack.pop()[0]
+		width := idx - prev_idx
+		area := prev_height * width
+
+		if largest < area {
+			largest = area
+		}
 	}
+
+	for len(stack) != 0 {
+		prev_idx := stack.peek()[0]
+		width := idx - prev_idx
+		prev_height := stack.pop()[1]
+		area := prev_height * width
+		if largest < area {
+			largest = area
+		}
+	}
+
+	return largest
 }
