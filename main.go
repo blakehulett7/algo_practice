17f7a66db42e2569bfb629a05a0137caa978802a
blakehulett7 <blake.hulett7@gmail.com>
2025-01-26 13:41:05 -0600

    practica

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"slices"
 )
 
 func main() {
@@ -12,59 +11,16 @@ func fmtin() {
 	fmt.Println()
 }
 
-type Stack [][]int
+func topKFrequent(nums []int, k int) []int {
+	counts := map[int]int{}
 
-func (s *Stack) push(num []int) {
-	*s = append(*s, num)
-}
-
-func (s *Stack) pop() []int {
-	top := (*s)[len(*s)-1]
-	*s = (*s)[:len(*s)-1]
-	return top
-}
-
-func (s Stack) peek() []int {
-	return s[len(s)-1]
-}
-
-func largestRectangleArea(heights []int) int {
-	largest := 0
-	stack := Stack{}
-
-	idx := 0
-	for idx < len(heights) {
-		height := heights[idx]
-		if len(stack) == 0 {
-			stack.push([]int{idx, height})
-			idx++
-			continue
-		}
-		prev_height := stack.peek()[1]
-		if prev_height <= height {
-			stack.push([]int{idx, height})
-			idx++
-			continue
-		}
-
-		prev_idx := stack.pop()[0]
-		width := idx - prev_idx
-		area := prev_height * width
-
-		if largest < area {
-			largest = area
-		}
+	for _, num := range nums {
+		counts[num]++
 	}
 
-	for len(stack) != 0 {
-		prev_idx := stack.peek()[0]
-		width := idx - prev_idx
-		prev_height := stack.pop()[1]
-		area := prev_height * width
-		if largest < area {
-			largest = area
-		}
+	buckets := make([][]int, len(nums)+1)
+	for num, count := range counts {
+		buckets[count] = append(buckets[count], num)
 	}
 
-	return largest
 }
