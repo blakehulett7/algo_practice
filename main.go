25a1a68014426dba1e0d0606e45d320c3a79d04d
blakehulett7 <blakehulett@Blake-Huletts-MacBook-Pro.local>
2025-01-27 11:42:51 -0600

    reps

--- main.go
+++ main.go
@@ -11,51 +11,16 @@ func fmtin() {
 	fmt.Println()
 }
 
-type Stack []rune
+func containsDuplicate(nums []int) bool {
+	seen := map[int]bool{}
 
-func (s *Stack) push(r rune) {
-	*s = append(*s, r)
-}
-
-func (s *Stack) pop() rune {
-	top := (*s)[len(*s)-1]
-	*s = (*s)[:len(*s)-1]
-	return top
-}
-
-func (s Stack) peek() rune {
-	return s[len(s)-1]
-}
-
-func isValid(s string) bool {
-	stack := Stack{}
-	valid := map[rune]rune{
-		'}': '{',
-		']': '[',
-		')': '(',
-	}
-
-	runes := []rune(s)
-	for _, r := range runes {
-		if len(stack) == 0 {
-			stack.push(r)
-			continue
+	for _, num := range nums {
+		if seen[num] {
+			return true
 		}
 
-		open, is_closer := valid[r]
-		if !is_closer {
-			stack.push(r)
-			continue
-		}
-
-		if stack.pop() != open {
-			return false
-		}
+		seen[num] = true
 	}
 
-	if len(stack) != 0 {
-		return false
-	}
-
-	return true
+	return false
 }
