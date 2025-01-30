d866f63d963890d569d8080384fe1241538384d2
blakehulett7 <blake.hulett7@gmail.com>
2025-01-30 16:25:10 -0600

    bst

--- main.go
+++ main.go
@@ -12,33 +12,26 @@ func fmtin() {
 	fmt.Println()
 }
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
 
-func mergeTwoLists(list1, list2 *ListNode) *ListNode {
-	dummy := &ListNode{}
-	node := dummy
+	for left <= right {
+		mid := (right-left)/2 + left
+		num := nums[mid]
 
-	for list1 != nil && list2 != nil {
-		if list1.Val < list2.Val {
-			node.Next = list1
-			list1 = list1.Next
-			node = node.Next
+		if num < target {
+			left = mid + 1
 			continue
 		}
 
-		node.Next = list2
-		list2 = list2.Next
-		node = node.Next
+		if num != target {
+			right = mid - 1
+			continue
+		}
+
+		return mid
 	}
 
-	if list2 == nil {
-		node.Next = list1
-		return dummy.Next
-	}
-
-	node.Next = list2
-	return dummy.Next
+	return -1
 }
