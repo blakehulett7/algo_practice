1ff37b0a394d4affda07093c7d498d3051da5692
blakehulett7 <blake.hulett7@gmail.com>
2025-01-31 17:59:32 -0600

    stonks

--- main.go
+++ main.go
@@ -12,31 +12,27 @@ func fmtin() {
 	fmt.Println()
 }
 
-func isPalindrome(s string) bool {
-	left := 0
-	right := len(s) - 1
-	runes := []rune(s)
+func maxProfit(prices []int) int {
+	buy_idx := 0
+	sell_idx := 1
+	max_profit := 0
 
-	for left < right {
-		left_char := unicode.ToLower(runes[left])
-		if !unicode.IsDigit(left_char) && !unicode.IsLetter(left_char) {
-			left++
+	for sell_idx < len(prices) {
+		buy := prices[buy_idx]
+		sell := prices[sell_idx]
+
+		if sell < buy {
+			buy_idx = sell_idx
+			sell_idx++
 			continue
 		}
 
-		right_char := unicode.ToLower(runes[right])
-		if !unicode.IsDigit(right_char) && !unicode.IsLetter(right_char) {
-			right--
-			continue
+		profit := sell - buy
+		if max_profit < profit {
+			max_profit = profit
 		}
-
-		if left_char != right_char {
-			return false
-		}
-
-		left++
-		right--
+		sell_idx++
 	}
 
-	return true
+	return max_profit
 }
