55d44c4a6a73d23c0abd69393c72765cb6a41b6e
blakehulett7 <blake.hulett7@gmail.com>
2025-01-31 18:15:50 -0600

    mergin

--- main.go
+++ main.go
@@ -17,18 +17,28 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func hasCycle(head *ListNode) bool {
-	seen := map[*ListNode]bool{}
-	current := head
+func mergeTwoLists(list1, list2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	node := dummy
 
-	for current != nil {
-		if seen[current] {
-			return true
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			node.Next = list1
+			list1 = list1.Next
+			node = node.Next
+			continue
 		}
 
-		seen[current] = true
-		current = current.Next
+		node.Next = list2
+		list2 = list2.Next
+		node = node.Next
 	}
 
-	return false
+	if list2 == nil {
+		node.Next = list1
+		return dummy.Next
+	}
+
+	node.Next = list2
+	return dummy.Next
 }
