73c5969fd43ec7fdd78e9317fb8ce704da582bc9
blakehulett7 <blake.hulett7@gmail.com>
2025-02-06 17:46:18 -0600

    actually can't believe that worked

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"unicode"
 )
 
@@ -18,10 +19,39 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func maxDepth(root *TreeNode) int {
-	if root == nil {
-		return 0
+func abs(i int) int {
+	if i < 0 {
+		return i * -1
+	}
+	return i
+}
+
+func isBalanced(root *TreeNode) bool {
+	var dfs func(*TreeNode) []int
+	dfs = func(root *TreeNode) []int {
+		if root == nil {
+			return []int{1, 0}
+		}
+
+		left := dfs(root.Left)
+		right := dfs(root.Right)
+
+		if left[0] != 1 || right[0] != 1 {
+			return []int{0, 0}
+		}
+
+		if abs(right[1]-left[1]) > 1 {
+			return []int{0, 0}
+		}
+
+		return []int{1, max(left[1], right[1]) + 1}
 	}
 
-	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+	is_balanced := dfs(root)[0]
+
+	if is_balanced != 1 {
+		return false
+	}
+
+	return true
 }
