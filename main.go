c0746f7b2e68fd504acf59c52002bd6269f6bfa8
blakehulett7 <blake.hulett7@gmail.com>
2025-02-07 17:52:54 -0600

    all the money

--- main.go
+++ main.go
@@ -13,30 +13,49 @@ func fmtin() {
 	fmt.Println()
 }
 
-func isPalindrome(s string) bool {
-	left := 0
-	right := len(s) - 1
+type Stack []rune
+
+func (s *Stack) push(r rune) {
+	*s = append(*s, r)
+}
+
+func (s *Stack) pop() rune {
+	top := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return top
+}
+
+func (s Stack) peek() rune {
+	return s[len(s)-1]
+}
+
+func isValid(s string) bool {
+	stack := Stack{}
+	valid_map := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
 	runes := []rune(s)
 
-	for left < right {
-		left_char := unicode.ToLower(runes[left])
-		if !unicode.IsDigit(left_char) && !unicode.IsLetter(left_char) {
-			left++
+	for _, r := range runes {
+		opener, is_closer := valid_map[r]
+		if !is_closer {
+			stack.push(r)
 			continue
 		}
 
-		right_char := unicode.ToLower(runes[right])
-		if !unicode.IsDigit(right_char) && !unicode.IsLetter(right_char) {
-			right--
-			continue
-		}
-
-		if left_char != right_char {
+		if len(stack) == 0 {
 			return false
 		}
 
-		left++
-		right--
+		if opener != stack.pop() {
+			return false
+		}
+	}
+
+	if len(stack) != 0 {
+		return false
 	}
 
 	return true
