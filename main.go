3772f799d6b6722f1f67d04b98f167eb96eeedd1
blakehulett7 <blake.hulett7@gmail.com>
2025-02-07 12:27:59 -0600

    dupin

--- main.go
+++ main.go
@@ -13,34 +13,16 @@ func fmtin() {
 	fmt.Println()
 }
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
+func containsDuplicate(nums []int) bool {
+	seen := map[int]bool{}
 
-func levelOrder(root *TreeNode) [][]int {
-	to_visit := []*TreeNode{root}
-	result := [][]int{}
-
-	for len(to_visit) != 0 {
-		list_size := len(to_visit)
-		list := []int{}
-		for i := 0; i < list_size; i++ {
-			current := to_visit[0]
-			to_visit = to_visit[1:]
-			if current == nil {
-				continue
-			}
-			to_visit = append(to_visit, current.Left, current.Right)
-			list = append(list, current.Val)
+	for _, num := range nums {
+		if seen[num] {
+			return true
 		}
 
-		if len(list) == 0 {
-			break
-		}
-		result = append(result, list)
+		seen[num] = true
 	}
 
-	return result
+	return false
 }
