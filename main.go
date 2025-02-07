5d605c8087b2168635a2498a74f7cdde114d0e3c
blakehulett7 <blake.hulett7@gmail.com>
2025-02-06 18:11:14 -0600

    that's the same tree!

--- main.go
+++ main.go
@@ -19,37 +19,21 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func abs(i int) int {
-	if i < 0 {
-		return i * -1
+func isSameTree(p, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
 	}
-	return i
-}
-
-func isBalanced(root *TreeNode) bool {
-	var dfs func(*TreeNode) []int
-	dfs = func(root *TreeNode) []int {
-		if root == nil {
-			return []int{1, 0}
-		}
-
-		left := dfs(root.Left)
-		right := dfs(root.Right)
-
-		if left[0] != 1 || right[0] != 1 {
-			return []int{0, 0}
-		}
-
-		if abs(right[1]-left[1]) > 1 {
-			return []int{0, 0}
-		}
-
-		return []int{1, max(left[1], right[1]) + 1}
+	if p == nil || q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
 	}
 
-	is_balanced := dfs(root)[0]
-
-	if is_balanced != 1 {
+	if !isSameTree(p.Left, q.Left) {
+		return false
+	}
+	if !isSameTree(p.Right, q.Right) {
 		return false
 	}
 
