8041486b6d0f938e21c442559691218a98982881
blakehulett7 <blake.hulett7@gmail.com>
2025-02-10 11:58:54 -0600

    balancing trees

--- main.go
+++ main.go
@@ -19,10 +19,39 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func maxDepth(root *TreeNode) int {
-	if root == nil {
-		return 0
+func isBalanced(root *TreeNode) bool {
+	var dfs func(*TreeNode) []int
+	dfs = func(node *TreeNode) []int {
+		if node == nil {
+			return []int{1, 0}
+		}
+
+		left := dfs(node.Left)
+		right := dfs(node.Right)
+
+		if left[0] == 0 || right[0] == 0 {
+			return []int{0, 0}
+		}
+
+		if abs(right[1]-left[1]) > 1 {
+			return []int{0, 0}
+		}
+
+		return []int{1, max(left[1], right[1]) + 1}
 	}
 
-	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+	is_balanced := dfs(root)
+	if is_balanced[0] == 0 {
+		return false
+	}
+
+	return true
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return i * -1
+	}
+
+	return i
 }
