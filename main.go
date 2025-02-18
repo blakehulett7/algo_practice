55437d8fb9b4303d0e60d458ac5986b9e8535ebd
blakehulett7 <blake.hulett7@gmail.com>
2025-02-18 11:50:38 -0600

    duping

--- main.go
+++ main.go
@@ -13,45 +13,16 @@ func fmtin() {
 	fmt.Println()
 }
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
+func containsDuplicate(nums []int) bool {
+	seen := map[int]bool{}
 
-func isBalanced(root *TreeNode) bool {
-	var dfs func(*TreeNode) []int
-	dfs = func(node *TreeNode) []int {
-		if node == nil {
-			return []int{1, 0}
+	for _, num := range nums {
+		if seen[num] {
+			return true
 		}
 
-		left := dfs(node.Left)
-		right := dfs(node.Right)
-
-		if left[0] == 0 || right[0] == 0 {
-			return []int{0, 0}
-		}
-
-		if abs(right[1]-left[1]) > 1 {
-			return []int{0, 0}
-		}
-
-		return []int{1, max(left[1], right[1]) + 1}
+		seen[num] = true
 	}
 
-	is_balanced := dfs(root)
-	if is_balanced[0] == 0 {
-		return false
-	}
-
-	return true
-}
-
-func abs(i int) int {
-	if i < 0 {
-		return i * -1
-	}
-
-	return i
+	return false
 }
